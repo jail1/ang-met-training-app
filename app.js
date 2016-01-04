18fde85695ce29e4170afc26c8ed53dff4956fe4
Silviu Iulian <iuliano2k@gmail.com>
2016-01-04 13:58:53 +0200

    Chapter 3 finished .3 way data binding.

--- app.js
+++ app.js
@@ -2,32 +2,33 @@
  * Created by silviu on 1/4/16.
  */
 
+Parties = new Mongo.Collection("parties");
+
 if(Meteor.isClient) {
 
 
     angular.module('socially', ['angular-meteor']);
 
-    angular.module('socially').controller('PartiesListCtrl', PartiesListCtrl);
+    angular.module('socially').directive('partiesList', partiesList);
 
-    PartiesListCtrl.$inject = ['$scope'];
+    function partiesList () {
+        return {
+            restrict        : 'E',
+            templateUrl     : 'parties-list.html',
+            controllerAs    : 'partiesList',
+            controller      : function($scope, $reactive) {
 
-    function PartiesListCtrl($scope) {
+                // # Bind the scope to the current context, and make it a reactive value.
+                $reactive(this).attach($scope);
 
-        $scope.parties = [
-            {
-                'name': 'Dubstep-Free Zone',
-                'description': 'Can we please just for an evening not listen to dubstep.'
-            },
-            {
-                'name': 'All dubstep all the time',
-                'description': 'Get it on!'
-            },
-            {
-                'name': 'Savage lounging',
-                'description': 'Leisure suit required. And only fiercest manners.'
+                this.helpers({
+                    parties : () => {
+                        return Parties.find({});
+                    }
+                });
+
             }
-        ];
-
+        }
     }
 
 }
